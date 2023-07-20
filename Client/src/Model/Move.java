a428351b2e2705033b07852cc25638e82f8a73cb
Mehrab Kalantary <103942008+MehrabKalantary@users.noreply.github.com>
2023-07-20 15:08:06 +0330

    all files

---
+++ Client/src/Model/Move.java
@@ -0,0 +1,14 @@
+package Model;
+
+/**
+ * Movement of Hero Sent to Server
+ */
+public enum Move {
+    UP,
+    DOWN,
+    RIGHT,
+    LEFT,
+    DiagonalUP,
+    DiagonalDown,
+    Still
+}
---
+++ Server/src/units/Hero/Power.java
@@ -0,0 +1,44 @@
+package units.Hero;
+
+import javafx.geometry.Point2D;
+
+public abstract class Power {
+
+    protected int level_;
+    protected int availableIn;
+
+    protected boolean active;
+    protected Power() {
+        level_ = 1;
+        active = false;
+    }
+
+    public void levelUp() {
+        if(level_ < 4)
+            level_++;
+    }
+
+    public abstract boolean inRange( Point2D point);
+
+    public abstract boolean isAvailable();
+
+    public abstract void turn();
+
+    public abstract int getMana_cost();
+
+    public void activate() {
+        if(this.isAvailable() && !this.active) {
+            active = true;
+        }
+    }
+
+    public void deActivate() {
+        active = false;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public abstract double getDamage();
+}
